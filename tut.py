6f929df1fe169409a4b1bc014d6cf0763cbb386c
BeXe <Balt.Elferink@rivm.nl>
2020-12-23 21:58:51 +0100

    Update tut.py
    Added correct gpio adresses

--- tut.py
+++ tut.py
@@ -7,27 +7,27 @@ button1=9                 # Button 1 is connected to physical pin 16
 button2=11                 # Button 2 is connected to physical pin 12
 button3=19
 button4=26
-button5=19
-button6=2
-button7=19
-button8=26
-button9=19
-button10=26
-button11=19
-button12=26
+button5=25
+button6=8
+button7=7
+button8=1
+button9=16
+button10=20
+button11=21
+#button12=
 
 LED1=24                    # LED 1 is connected to physical pin 22
 LED2=23                    # LED 2 is connected to physical pin 18
 LED3=18
 LED4=15
 LED5=14
-LED6=18
-LED7=15
-LED8=14
-LED9=18
-LED10=15
-LED11=14
-LED11=12
+LED6=22
+LED7=27
+LED8=17
+LED9=13
+LED10=6
+LED11=5
+#LED12=
 
 GPIO.setup(button1,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button1 an input, Activate Pull UP Resistor
 GPIO.setup(button2,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 2 an input, Activate Pull Up Resistor
@@ -40,7 +40,7 @@ GPIO.setup(button8,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 8 an input, A
 GPIO.setup(button9,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 9 an input, Activate Pull UP Resistor
 GPIO.setup(button10,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 10 an input, Activate Pull Up Resistor
 GPIO.setup(button11,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 11 an input, Activate Pull UP Resistor
-GPIO.setup(button12,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 12 an input, Activate Pull Up Resistor
+#GPIO.setup(button12,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 12 an input, Activate Pull Up Resistor
 
 GPIO.setup(LED1,GPIO.OUT,) # Make LED 1 an Output
 GPIO.setup(LED2,GPIO.OUT,)  # Make LED 2 an Output
@@ -52,8 +52,8 @@ GPIO.setup(LED7,GPIO.OUT,) # Make LED 7 an Output
 GPIO.setup(LED8,GPIO.OUT,)  # Make LED 8 an Output
 GPIO.setup(LED9,GPIO.OUT,) # Make LED 9 an Output
 GPIO.setup(LED10,GPIO.OUT,)  # Make LED 10 an Output
-GPIO.setup(LED11,GPIO.OUT,) # Make LED 11 an Output
-GPIO.setup(LED12,GPIO.OUT)  # Make LED 12 an Output
+GPIO.setup(LED11,GPIO.OUT) # Make LED 11 an Output
+#GPIO.setup(LED12,GPIO.OUT)  # Make LED 12 an Output
 
 BS1=False                  # Set Flag BS1 to indicate LED is initially off
 BS2=False                  # Set Flag BS2 to indicate LED is initially off
@@ -66,7 +66,7 @@ BS8=False                  # Set Flag BS4 to indicate LED is initially off
 BS9=False                  # Set Flag BS1 to indicate LED is initially off
 BS10=False                  # Set Flag BS2 to indicate LED is initially off
 BS11=False                  # Set Flag BS3 to indicate LED is initially off
-BS12=False                  # Set Flag BS4 to indicate LED is initially off
+#BS12=False                  # Set Flag BS4 to indicate LED is initially off
 
 while(1):                  # Create an infinite Loop
         if GPIO.input(button1)==0:            # Look for button 1 press
@@ -179,13 +179,13 @@ while(1):                  # Create an infinite Loop
                         GPIO.output(LED11,False)
                         BS11=False
                         sleep(.5)
-        if GPIO.input(button12)==0: #Repeat above for LED 4 and button 4
-                print ("Button 12 Was Pressed:")
-                if BS12==False:
-                        GPIO.output(LED12,True)
-                        BS12=True
-                        sleep(.5)
-                else:
-                        GPIO.output(LED12,False)
-                        BS12=False
-                        sleep(.5)
+#        if GPIO.input(button12)==0: #Repeat above for LED 4 and button 4
+#                print ("Button 12 Was Pressed:")
+#                if BS12==False:
+#                        GPIO.output(LED12,True)
+#                        BS12=True
+#                        sleep(.5)
+#                else:
+#                        GPIO.output(LED12,False)
+#                        BS12=False
+#                        sleep(.5)
