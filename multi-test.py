a6beb9365576c2ccdc98b18b93f340d154fbb6bb
BeXe <Balt.Elferink@rivm.nl>
2021-01-02 21:01:09 +0100

    Update multi-test.py

--- multi-test.py
+++ multi-test.py
@@ -1,161 +1,75 @@
-from time import sleep     # Import sleep Library
-import RPi.GPIO as GPIO    # Import GPIO Library 
-#GPIO.setmode(GPIO.BOARD)   # Use Physical Pin Numbering Scheme
-GPIO.setmode(GPIO.BCM)
+#!/usr/bin/python
 
-# FOR 7 Segment display
+from time import sleep
+import RPi.GPIO as GPIO
+import subprocess
 import time
-import board
-import busio
-from adafruit_ht16k33 import segments
+import thread
 
-# FOR VIDEO
-import os
-import sys
-import logging
-from subprocess import Popen, PIPE, DEVNULL
+GPIO.setmode (GPIO.BCM)
+GPIO.setwarnings (False)
 
-logger = logging.getLogger(__name__)
+GPIO.setup(9, GPIO.IN)
+GPIO.setup(10, GPIO.IN)
+GPIO.setup(11, GPIO.IN)
 
-# Create the I2C interface.
-i2c = busio.I2C(board.SCL, board.SDA)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
 
-# Create the LED segment class.
-# This creates a 7 segment 4 character display:
-display = segments.Seg7x4(i2c)
+def welcome_loop():
+    while True:
+            global playProcess
+            x = 1
+            print "LOOPING"
+            time.sleep(.5)
+            playProcess=subprocess.Popen(['omxplayer','-b','Desktop/videos/loop/loop.mp4'],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE, close_fds=True)
+            time.sleep(10)
+            playProcess.stdin.write('q')
+            x += 1
 
+def videos():
+    while True:
+            if GPIO.input(9):
+                    print "STOP LOOP"
+                    time.sleep(.5)
+                    playProcess.stdin.write('q')
+                    time.sleep(.5)
+                    print "Play Sippycup"
+                    sippycup_video=subprocess.Popen(['omxplayer','-b','Desktop/videos/sippycup.mp4'],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE, close_fds=True)
+                    time.sleep(10)
+                    sippycup_video.stdin.write('q')
+                    time.sleep(.5)
+                    welcome_loop()
 
-# Button Variables connected to which GPIO pin.
-button1=9                 # Button 1 is connected to physical pin 16
-button2=11                 # Button 2 is connected to physical pin 12
-button3=19
-button4=26
-button5=25
-button6=8
-button7=7
-button8=1
-button9=20
-button10=16
-button11=21
-#button12=
+            if GPIO.input(10):
+                    print "STOP LOOP"
+                    time.sleep(.5)
+                    playProcess.stdin.write('q')
+                    time.sleep(.5)
+                    print "Play Dodgeballs"
+                    dodgeballs_video=subprocess.Popen(['omxplayer','-b','Desktop/videos/dodgeballs.mp4'],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE, close_fds=True)
+                    time.sleep(10)
+                    dodgeballs_video.stdin.write('q')
+                    time.sleep(.5)
+                    welcome_loop()
 
-# Led variables, connected tot which GPIO pin
-LED1=24                    # LED 1 is connected to physical pin 22
-LED2=23                    # LED 2 is connected to physical pin 18
-LED3=18
-LED4=15
-LED5=14
-LED6=22
-LED7=27
-LED8=17
-LED9=13
-LED10=6
-LED11=5
-#LED12=
+            if GPIO.input(11):
+                    print "STOP LOOP"
+                    time.sleep(.5)
+                    playProcess.stdin.write('q')
+                    time.sleep(.5)
+                    print "Play Shoppingcart"
+                    shoppingcart_video=subprocess.Popen(['omxplayer','-b','Desktop/videos/shoppingcart.mp4'],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE, close_fds=True)
+                    time.sleep(10)
+                    shoppingcart_video.stdin.write('q')
+                    time.sleep(.5)
+                    welcome_loop()
 
-# Movie Variables (set where movie lives)
-movie1 = ("/home/pi/movie/aurora.mp4")
-movie2 = ("/home/pi/Videos/movie2.mp4")
+thread.start_new_thread( videos, () )
+thread.start_new_thread( welcome_loop, () )
 
-m1=False
+while True:
+    pass
 
-# last state variable
-# last_state1 = True
-# last_state2 = True
-
-# 
-# input_state1 = True
-# input_state2 = True
-# quit_video = True
-
-# player = False
-
-GPIO.setup(button1,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button1 an input, Activate Pull UP Resistor
-GPIO.setup(button2,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 2 an input, Activate Pull Up Resistor
-GPIO.setup(button3,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 3 an input, Activate Pull UP Resistor
-GPIO.setup(button4,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 4 an input, Activate Pull Up Resistor
-GPIO.setup(button5,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 5an input, Activate Pull UP Resistor
-GPIO.setup(button6,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 6 an input, Activate Pull Up Resistor
-GPIO.setup(button7,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 7 an input, Activate Pull UP Resistor
-GPIO.setup(button8,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 8 an input, Activate Pull Up Resistor
-GPIO.setup(button9,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 9 an input, Activate Pull UP Resistor
-GPIO.setup(button10,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 10 an input, Activate Pull Up Resistor
-GPIO.setup(button11,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 11 an input, Activate Pull UP Resistor
-#GPIO.setup(button12,GPIO.IN,pull_up_down=GPIO.PUD_UP) # Make button 12 an input, Activate Pull Up Resistor
-
-GPIO.setup(LED1,GPIO.OUT,) # Make LED 1 an Output
-GPIO.setup(LED2,GPIO.OUT,)  # Make LED 2 an Output
-GPIO.setup(LED3,GPIO.OUT,) # Make LED 3 an Output
-GPIO.setup(LED4,GPIO.OUT,)  # Make LED 4 an Output
-GPIO.setup(LED5,GPIO.OUT,) # Make LED 5 an Output
-GPIO.setup(LED6,GPIO.OUT,)  # Make LED 6 an Output
-GPIO.setup(LED7,GPIO.OUT,) # Make LED 7 an Output
-GPIO.setup(LED8,GPIO.OUT,)  # Make LED 8 an Output
-GPIO.setup(LED9,GPIO.OUT,) # Make LED 9 an Output
-GPIO.setup(LED10,GPIO.OUT,)  # Make LED 10 an Output
-GPIO.setup(LED11,GPIO.OUT) # Make LED 11 an Output
-#GPIO.setup(LED12,GPIO.OUT)  # Make LED 12 an Output
-
-BS1=False                  # Set Flag BS1 to indicate LED is initially off
-BS2=False                  # Set Flag BS2 to indicate LED is initially off
-BS3=False                  # Set Flag BS3 to indicate LED is initially off
-BS4=False                  # Set Flag BS4 to indicate LED is initially off
-BS5=False                  # Set Flag BS1 to indicate LED is initially off
-BS6=False                  # Set Flag BS2 to indicate LED is initially off
-BS7=False                  # Set Flag BS3 to indicate LED is initially off
-BS8=False                  # Set Flag BS4 to indicate LED is initially off
-BS9=False                  # Set Flag BS1 to indicate LED is initially off
-BS10=False                  # Set Flag BS2 to indicate LED is initially off
-BS11=False                  # Set Flag BS3 to indicate LED is initially off
-#BS12=False                  # Set Flag BS4 to indicate LED is initially offwhile True:
- 
-
-   
-for key in get_key_events(): # get GPIO input
-        if key == '9':
-           player = players[0]
-        elif key == 'space':
-           player.toggle() # pause/unpause
-        elif key == 'enter':
-           player.start()
-
-import logging
-from subprocess import Popen, PIPE, DEVNULL
-
-logger = logging.getLogger(__name__)
-
-class player:
-    def __init__(self, movie):
-       self.movie = movie
-       self.process = None
-
-    def start(self):
-        self.stop()
-        self.process = Popen(['omxplayer', self.movie], stdin=PIPE,
-                             stdout=DEVNULL, close_fds=True, bufsize=0)
-        self.process.stdin.write(start_command) # start playing
-
-    def stop(self):
-        p = self.process
-        if p is not None:
-           try:
-               p.stdin.write(quit_command) # send quit command
-               p.terminate()
-               p.wait() # -> move into background thread if necessary
-           except EnvironmentError as e:
-               logger.error("can't stop %s: %s", self.movie, e)
-           else:
-               self.process = None
-
-    def toggle(self):
-        p = self.process
-        if p is not None:
-           try:
-               p.stdin.write(toggle_command) # pause/unpause
-           except EnvironmentError as e:
-               logger.warning("can't toggle %s: %s", self.movie, e)
-             
-names = 'sippycup', 'dodgeballs', 'shoppingcart'
-movies = ['home/pi/movie/{name}.mp4'.format(name=name) for name in names]
-players = [Player(movie=movie) for movie in movies]
-player = players[0]   
+GPIO.cleanup()
