443a1dd0bc2c6eb55cde712d8c1a29b45870e438
BeXe <Balt.Elferink@rivm.nl>
2021-01-02 22:51:13 +0100

    Create thread-example.py

---
+++ thread-example.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode (GPIO.BOARD)
+import threading
+from threading import Thread
+
+def firstLED():
+        i=0
+        blinks=5
+        while (i < blinks):
+
+                GPIO.setup (24, GPIO.OUT)
+                GPIO.output (24, GPIO.HIGH)
+                time.sleep(0.1)
+                GPIO.output (24, GPIO.LOW)
+                time.sleep(0.1)
+
+                i=i+1
+        return;
+
+def secondLED():
+        i=0
+        blinks=3
+        while (i < blinks):
+
+                GPIO.setup (23, GPIO.OUT)
+                GPIO.output (23, GPIO.HIGH)
+                time.sleep(0.1)
+                GPIO.output (23, GPIO.LOW)
+                time.sleep(0.1)
+
+                i=i+1
+        return;
+		
+if __name__=='__main__':
+    first_thread = Thread(target = firstLED)
+    second_thread = Thread(target = secondLED)
+    first_thread.start()
+    second_thread.start()
+
+    #DO STUFF HERE INSTEAD OF JUST WAITING?
+       
+    #wait for threads to finish
+    first_thread.join()
+    second_thread.join()
+
+    print "All done"
+    GPIO.cleanup()
