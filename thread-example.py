67c636aeff5637859b00aff236a51f040cceba7b
BeXe <Balt.Elferink@rivm.nl>
2021-01-05 21:41:01 +0100

    Update thread-example.py

--- thread-example.py
+++ thread-example.py
@@ -132,7 +132,7 @@ def buttonLOOP():
                                 time.sleep(.5)             # Delay
                                 thread.run(thread1)
                                # thread.run(thread2)
-                                countdown
+                                countdown.init()
                                # thread2.run(thread4)
                                 
                                                                 
