8dba5b9767f14aa77b591b1cb8104fb4c569dd95
BeXe <Balt.Elferink@rivm.nl>
2021-01-06 20:53:50 +0100

    Update loops.py

--- loops.py
+++ loops.py
@@ -229,10 +229,10 @@ class MyApp(App):
 		outputControl = ToggleButton(text="LED")
 		outputControl.bind(on_press=press_callback)
 		beepButton = Button(text="BEEP!")
-		beepButton.bind(on_press=press_callback)
+		#beepButton.bind(on_press=press_callback)
 		wimg = Image(source='logo.png')
 		speedSlider = Slider(orientation='vertical', min=1, max=30, value=speed)
-		speedSlider.bind(on_touch_down=update_speed, on_touch_move=update_speed)
+		#speedSlider.bind(on_touch_down=update_speed, on_touch_move=update_speed)
 
 		# Add the UI elements to the layout:
 		layout.add_widget(wimg)
