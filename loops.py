5fa8cb7ed929a8e85b667e86cdec8d79e763cc3e
BeXe <Balt.Elferink@rivm.nl>
2021-01-06 20:49:07 +0100

    Update loops.py

--- loops.py
+++ loops.py
@@ -13,6 +13,18 @@ import os
 import sys
 from subprocess import Popen
 
+import kivy
+kivy.require('1.1.0')
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.image import Image
+from kivy.uix.slider import Slider
+from kivy.clock import Clock
+from kivy.graphics import Color, Rectangle
+
 # Create the I2C interface.
 i2c = busio.I2C(board.SCL, board.SDA)
 
@@ -182,8 +194,49 @@ def buttonLOOP2():
                                 GPIO.output (24, GPIO.LOW)
                                 time.sleep(1)
                                 i=i+1
+
+                            
+class MyApp(App):
+
+	def build(self):
+		# Set up the layout:
+		layout = GridLayout(cols=5, spacing=30, padding=30, row_default_height=150)
+
+		# Make the background gray:
+		with layout.canvas.before:
+			Color(.2,.2,.2,1)
+			self.rect = Rectangle(size=(800,600), pos=layout.pos)
+
+		# Instantiate the first UI object (the GPIO input indicator):
+		inputDisplay = InputButton(text="Input")
+
+		# Schedule the update of the state of the GPIO input button:
+		Clock.schedule_interval(inputDisplay.update, 1.0/10.0)
+
+		# Create the rest of the UI objects (and bind them to callbacks, if necessary):
+		outputControl = ToggleButton(text="LED")
+		outputControl.bind(on_press=press_callback)
+		beepButton = Button(text="BEEP!")
+		beepButton.bind(on_press=press_callback)
+		wimg = Image(source='logo.png')
+		speedSlider = Slider(orientation='vertical', min=1, max=30, value=speed)
+		speedSlider.bind(on_touch_down=update_speed, on_touch_move=update_speed)
+
+		# Add the UI elements to the layout:
+		layout.add_widget(wimg)
+		layout.add_widget(inputDisplay)
+		layout.add_widget(outputControl)
+		layout.add_widget(beepButton)
+		layout.add_widget(speedSlider)
+
+		# Start flashing the LED
+		Clock.schedule_once(flash, 1.0/speed)
+
+		return layout                            
+                            
 if __name__=='__main__':
-     
+    MyApp().run()
+       
     button_thread = Thread(target = buttonLOOP)
     first_thread = Thread(target = buttonLOOP2)
     
