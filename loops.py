caad8a9157efb941d8c6762f5c27df1037e893ca
BeXe <Balt.Elferink@rivm.nl>
2021-01-06 21:10:40 +0100

    Update loops.py

--- loops.py
+++ loops.py
@@ -13,6 +13,23 @@ import os
 import sys
 from subprocess import Popen
 
+import kivy
+kivy.require('1.0.6') # replace with your current kivy version !
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text='Hello world')
+
+
+
+
+
+
 # Create the I2C interface.
 i2c = busio.I2C(board.SCL, board.SDA)
 
@@ -185,6 +202,7 @@ def buttonLOOP2():
                           
                             
 if __name__=='__main__':
+     MyApp().run()
          
     button_thread = Thread(target = buttonLOOP)
     first_thread = Thread(target = buttonLOOP2)
