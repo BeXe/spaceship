4e3935470ed5aa7321c3b44acc4dd9114dbeecb6
BeXe <Balt.Elferink@rivm.nl>
2021-01-06 13:56:39 +0100

    Update loops.py

--- loops.py
+++ loops.py
@@ -117,6 +117,7 @@ def buttonLOOP():
         while(1):                  # Create an infinite Loop
                 if GPIO.input(button1)==0:            # Look for button 1 press
                         print ("Button 1 Was Pressed:")
+                        time.sleep(5)             # Delay
                         for i in range(10, -1, -1):
                                    print('{num:06d}'.format(num=i))
                                    display.fill(0)
